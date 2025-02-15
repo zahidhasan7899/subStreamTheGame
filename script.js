57ad2e2880d4d67536b36b76d55d06e6f95f9fa2
zahidhasan7899 <zahid2hasan.uniapp@gmail.com>
2025-02-16 03:26:21 +0600

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 document.addEventListener("DOMContentLoaded", function () {
     var video = document.getElementById("live-stream");
-    var streamUrl = "https://dffgtrda.vffddd.online:8181/bein1/index.m3u8";
+    var streamUrl = "https://dffgtrda.vffddd.online:8181/bein1/tracks-v1a1/mono.ts.m3u8";
 
     if (Hls.isSupported()) {
         var hls = new Hls();
