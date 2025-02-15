0b8cb7a26265ac81af9559818c800355e9e6402a
zahidhasan7899 <zahid2hasan.uniapp@gmail.com>
2025-02-16 02:11:50 +0600

    Update script.js

--- script.js
+++ script.js
@@ -1,16 +1,18 @@
 document.addEventListener("DOMContentLoaded", function () {
-    var video = document.getElementById("live-stream");
-    var streamUrl = "https://dai.google.com/ssai/event/8cov9tHQRj26xm-Qz6uWzQ/master.m3u8";
+    var video = document.getElementById("video");
+    var videoSrc = "https://clicknboyn.xyz/live/n-2/chunks.m3u8";
 
     if (Hls.isSupported()) {
         var hls = new Hls();
-        hls.loadSource(streamUrl);
+        hls.loadSource(videoSrc);
         hls.attachMedia(video);
         hls.on(Hls.Events.MANIFEST_PARSED, function () {
             video.play();
         });
-    } else if (video.canPlayType("application/x-mpegURL")) {
-        video.src = streamUrl;
-        video.play();
+    } else if (video.canPlayType("application/vnd.apple.mpegurl")) {
+        video.src = videoSrc;
+        video.addEventListener("loadedmetadata", function () {
+            video.play();
+        });
     }
 });
